2e28bcb8e8d307448c79a2416c3476a8485275e1
boboinRL <110905292+boboinRL@users.noreply.github.com>
2026-03-06 02:38:17 +0000

    Time: 13 ms (77.13%), Space: 12.5 MB (99.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,18 @@
 class Solution(object):
     def isPalindrome(self, s):
-        l,r = 0, len(s)-1
-        while l<r:
-            while l<r and not s[l].isalnum():
-                l += 1
-            while l<r and not s[r].isalnum():
+        L, r = 0, len(s)-1
+        while L < r:          # 2. 没碰头就继续
+        # 3. 过滤掉左边的杂质
+            while L < r and not s[L].isalnum():
+                L += 1
+        # 3. 过滤掉右边的杂质
+            while L < r and not s[r].isalnum():
                 r -= 1
-            if s[l].lower() != s[r].lower():
+        # 4. 对比
+            if s[L].lower() != s[r].lower():
                 return False
-            l += 1
+        # 5. 各自推进一步
+            L += 1
             r -= 1
+        # 如果循环走完了都没返回 False，说明全对上了
         return True
-             
